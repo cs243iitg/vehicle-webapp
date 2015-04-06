04b0338edab7d13dcfcbb715485e45fa29ff88a0
karnatijayadeep <karnatijayadeep@gmail.com>
2015-04-06 19:20:23 +0000

    Added Django's default authentication
    - Users can be registered only by admin,
    - Users and admin can login to home and logout.

--- webapp/vms/views.py
+++ webapp/vms/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import auth
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+#------------------------------------------------------------
+#       User Authentication
+#------------------------------------------------------------
+
+def login(request):
+    """
+    displays login page at the start
+    """
+    c = {}
+    c.update(csrf(request))
+    if request.method == 'POST':
+        return render_to_response('vms/login.html', {'form_errors': form_errors})
+    else:
+        return render_to_response('vms/login.html', c)
+
+
+@login_required(login_url="/vms")
+def logout(request):
+    """
+    logs out user, only if he is already logged in.
+    """
+    auth.logout(request)
+    return HttpResponseRedirect('/vms/', {'form_errors': "You've succesfully logged out."})
+
+
+def auth_view(request):
+    """
+    Authenticates user from the username and password from POST
+    """
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        # Since the user is authenticated, Log the user in.
+        auth.login(request, user)
+        return HttpResponseRedirect('/vms/home')
+
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponseRedirect('/vms/')
+
+#------------------------------------------------------------
+
+@login_required(login_url="/vms/")
+def home(request):
+    """
+    displays home page for user, with his previous tasks
+    """
+    return render_to_response('vms/home.html',
+                            {'username': request.user.username,},
+                            context_instance=RequestContext(request))
